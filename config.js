c010642a5375ea1298292872936df631c3a07bbf
Maria-Luisa1599 <98129683+Maria-Luisa1599@users.noreply.github.com>
2022-12-13 18:23:13 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-khUVuM3g9gta3bDEtsB-SDWW3PKThJo",
+  authDomain: "projetoautoral-27954.firebaseapp.com",
+  databaseURL: "https://projetoautoral-27954-default-rtdb.firebaseio.com",
+  projectId: "projetoautoral-27954",
+  storageBucket: "projetoautoral-27954.appspot.com",
+  messagingSenderId: "261538410880",
+  appId: "1:261538410880:web:696bbf320837f03796d165"
+};
---
+++ navigation/tabNavigator.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import WeatherDetails from "../screens/weatherDetails";
+import Home from "../screens/home";
+
+const Tab = createBottomTabNavigator()
+
+export default class TabNavigator extends React.Component{
+    render(){
+        return(
+            <Tab.Navigator labeled={false}>
+                <Tab.Screen name="Home" component={Home}/>
+                <Tab.Screen name="WeatherDetails" component={WeatherDetails}/>
+            </Tab.Navigator>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+});
+
+//Ajeitar o Tab, colocar icones e ajustar os estilos da tela
